3776ff557ddd3cbe00ac40bc074908a76749983f
Darlan Borges <darlanxw@gmail.com>
2015-03-03 11:52:23 -0300

    Estrutura do Projeto
    API + Client

---
+++ EventoAspNetBr/EventoAspNetBr/Api/Controllers/CommunicationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web.Http;
+using EventoAspNetBr.Api.Model;
+
+namespace EventoAspNetBr.Api.Controllers
+{
+    public class CommunicationController : ApiController
+    {
+        private readonly AspNetBrContext _context;
+
+        public CommunicationController()
+        {
+            _context = new AspNetBrContext();
+        }
+
+        public dynamic Post(Communication communication)
+        {
+            try
+            {
+                _context.Communications.Add(communication);
+                _context.SaveChanges();
+
+                return new {success = true, data = communication};
+            }
+            catch (Exception e)
+            {
+                return new { success = false, error = e };   
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EventoAspNetBr/EventoAspNetBr/app/app.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', [
+        'ngRoute',
+        'ui.bootstrap'
+    ]);
+
+    app.run(['$rootScope', '$route', '$location',
+        function ($rootScope, $route, $location) {
+        }]);
+})();
+
