6c0f940c189469b7324b6686f1c11d721a6348d8
Darlan Borges <darlanxw@gmail.com>
2015-03-04 14:47:31 -0300

    Alteração do logo.
    Alteração do logotipo e exclusão da api.

--- Project/EventoAspNetBr/EventoAspNetBr/Api/ViewModel/ContactViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EventoAspNetBr.Api.ViewModel
-{
-    public class ContactViewModel
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Message { get; set; }
-    }
-}
